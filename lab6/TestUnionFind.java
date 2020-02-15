aed3c9570ac6cbb1327c309263c0a622db5d2107
liyu <liyr@zhong-ying.com>
2020-02-15 20:00:35 +0800

    未做测试

---
+++ lab6/TestUnionFind.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestUnionFind {
+
+    @Test
+    public void testBasic(){
+
+    }
+
+}
--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,35 +1,39 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class UnionFind {
 
     // TODO - Add instance variables?
+    private int[] parent;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        throw new IllegalArgumentException();
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        return parent[find(v1)];
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        return parent[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -38,14 +42,37 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        int i = find(v1);
+        int j = find(v2);
+
+        if (parent[i] > parent[j]){
+            int size = parent[j];
+            parent[j] = i;
+            parent[i] += size;
+
+        }else {
+            int size = parent[i];
+            parent[i] = j;
+            parent[j] += size;
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+
+        List<Integer> alongPath = new ArrayList<>();
+
+        while (parent[vertex] >= 0) {
+            alongPath.add(parent[vertex]);
+            vertex = parent[vertex];
+        }
+
+        for (Integer item: alongPath){
+            parent[item] = vertex;
+        }
+
+        return vertex;
     }
 
 }
